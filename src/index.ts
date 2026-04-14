1d17f068bed256ae1e6c980d098b321af2218821
bschack <benharris1102@gmail.com>
2026-04-14 00:05:43 -0400

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Hono } from 'hono'
 import data from "../data/cv-data.json" with { type: "json" };
+import { cors } from 'hono/cors';
 
 const app = new Hono()
+
+// enable CORS
+app.use(cors())
 
 app.get('/', (c) => {
   return c.json(data)
